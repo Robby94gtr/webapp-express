8b135b8c691fb57bfcff95f8e7c3e311999a856d
Robby94gtr <robertogramazio94@gmail.com>
2025-06-05 20:31:29 +0200

    [UPD] Star

--- controllers/movieControllers.js
+++ controllers/movieControllers.js
@@ -46,7 +46,7 @@ function show(req, res) {
                     return res.status(500).json({ error: 'Errore nel calcolo della media delle recensioni', details: err.message });
                 }
                 if (mediaResults.length > 0) {
-                    movie.average_rating = mediaResults[0].average_rating || 0;
+                    movie.average_rating = Math.round(mediaResults[0].average_rating) || 0;
                 } else {
                     movie.average_rating = 0;
                 }
